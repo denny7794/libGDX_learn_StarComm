0bbaf340a99d6af848443b5e5fc62d829449eaae
denny7794 <denny7794@gmail.com>
2016-11-10 12:23:02 +0400

    привязка размера звезд к их скорости

--- core/src/com/starcomm/game/Background.java
+++ core/src/com/starcomm/game/Background.java
@@ -8,15 +8,17 @@ public class Background {
     class Star {
         Vector2 position; // x y
         float speed;
+        private int size;
 
         public Star() {
             this.position = new Vector2((float)(Math.random() * 1280), (float)(Math.random() * 720));
-            this.speed = 1.0f + (float)(Math.random() * 10);
+            this.speed = 0.1f + (float)(Math.random() * 5.0f);
+            size = (int)(5 * (this.speed));
         }
 
         public void update() {
             position.x -= speed;
-            if(position.x < 0)
+            if (position.x < 0)
                 position.x = 1350;
         }
     }
@@ -24,11 +26,12 @@ public class Background {
     private Texture texture;
     private Texture texStar;
     private Star[] stars;
+    private final int STARS_COUNT = 200;
 
     public Background() {
         texture = new Texture("bg.png");
         texStar = new Texture("star12.tga");
-        stars = new Star[300];
+        stars = new Star[STARS_COUNT];
         for (int i = 0; i < stars.length; i++) {
             stars[i] = new Star();
         }
@@ -37,7 +40,8 @@ public class Background {
     public void render(SpriteBatch batch) {
         batch.draw(texture, 0, 0);
         for (int i = 0; i < stars.length; i++) {
-            batch.draw(texStar, stars[i].position.x, stars[i].position.y);
+            //batch.draw(texStar, stars[i].position.x, stars[i].position.y);
+            batch.draw(texStar, stars[i].position.x, stars[i].position.y, stars[i].size, stars[i].size);
         }
     }
 
